63eb19ba155d339085c32088f66db67905fee8bc
Ryan Murray <ryanmurrayemail@gmail.com>
2020-06-02 21:00:17 +1000

    mobile layouts fixed for homepage

--- src/js/script.js
+++ src/js/script.js
@@ -3,4 +3,10 @@ jQuery(document).ready(function(){
         $(this).find('img').toggleClass('rotate-90');
         $(this).next('.panel').slideToggle();
     });
+
+    $('.hamburger').on('click', function(){
+        $(this).toggleClass('is-active');
+
+        $('.mobileNav').slideToggle();
+    });
 });
