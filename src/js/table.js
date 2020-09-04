447f51c45c97b2aa113fdfd387e6e065884f36b2
Ryan Murray <ryanmurrayemail@gmail.com>
2020-09-04 10:37:29 +1000

    ran all updates and added pagination to table layout

--- src/js/table.js
+++ src/js/table.js
@@ -20,4 +20,27 @@ jQuery(document).ready(function($){
         }
     }
     function getCellValue(row, index){ return $(row).children('td').eq(index).text() }
+
+    $('table.paginated').each(function() {
+        var currentPage = 0;
+        var numPerPage = 10;
+        var $table = $(this);
+        $table.bind('repaginate', function() {
+            $table.find('tbody tr').hide().slice(currentPage * numPerPage, (currentPage + 1) * numPerPage).show();
+        });
+        $table.trigger('repaginate');
+        var numRows = $table.find('tbody tr').length;
+        var numPages = Math.ceil(numRows / numPerPage);
+        var $pager = $('<div class="pager mt-8 w-full"></div>');
+        for (var page = 0; page < numPages; page++) {
+            $('<span class="page-number p-2 bg-light mr-2 cursor-pointer hover:bg-primary-500 rounded"></span>').text(page + 1).bind('click', {
+                newPage: page
+            }, function(event) {
+                currentPage = event.data['newPage'];
+                $table.trigger('repaginate');
+                $(this).addClass('active bg-primary-500 text-white').siblings().removeClass('active bg-primary-500 text-white');
+            }).appendTo($pager).addClass('clickable');
+        }
+        $pager.insertAfter($table).find('span.page-number:first').addClass('active');
+    });
 });
